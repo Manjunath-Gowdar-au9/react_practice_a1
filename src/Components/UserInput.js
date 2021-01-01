ee4ea5cbf64b738f6df50adcd0bed328e766e4bc
Manjunath Gowdar <57346550+Manjunath-Gowdar-au9@users.noreply.github.com>
2021-01-01 22:20:14 +0530

    input event handler with inline style

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from "react";
 
+
 const UserInput = (props) => {
+  // inline style
+  const inlineStyle = {
+    border: '2px solid red'
+  }
   return (
     <Fragment>
-      <input type="text" onChange={props.changed} value={props.value}/>
+      <input type="text" style={inlineStyle} onChange={props.changed} value={props.value}/>
     </Fragment>
   );
 };
